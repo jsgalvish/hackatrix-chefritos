0e7c2d7e136f7d7c5b1946b18d9eb62519912b08
jsgalvish <jsgalvish@unal.edu.co>
2019-09-28 09:14:50 -0500

    set database firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule} from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+	imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+		AngularFireModule.initializeApp(environment.firebaseConfig),
+		AngularFirestoreModule
+	],
   providers: [
     StatusBar,
     SplashScreen,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+	production: false,
+	firebaseConfig : {
+    apiKey: "AIzaSyBVYvZaudRI2tmx9geXVohZ3YeS3vi2a_c",
+    authDomain: "chefritos-4664f.firebaseapp.com",
+    databaseURL: "https://chefritos-4664f.firebaseio.com",
+    projectId: "chefritos-4664f",
+    storageBucket: "chefritos-4664f.appspot.com",
+    messagingSenderId: "793602332762",
+    appId: "1:793602332762:web:b8b29c4c19cfd3d6c42063"
+  }
 };
 
 /*
